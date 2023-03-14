0d76a92eff30561b6f13faa45b0efb6469364062
Mel <jones.mel@gmail.com>
2023-03-14 17:21:12 +0100

    test: instantiate a class that doesn't exist and expect top toBe -1

--- tdd-practice/test/stack.test.js
+++ tdd-practice/test/stack.test.js
@@ -1,5 +1,13 @@
 describe('My Stack', () => {
-  it.todo('is created empty')
+  it('is created empty', () => {
+    // instantiate a class that doesn't exist to represent the object we're testing
+    const stack = new Stack()
+
+    // make sure our code does whats expected
+    // because top is empty when it's first created the value should be -1
+    // toBe is a jest matcher
+    expect(stack.top).toBe(-1)
+  })
 
   it.todo('can push to the top')
 
