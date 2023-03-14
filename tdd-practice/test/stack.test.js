de33087ff1a6e3740cde33791cf6649f4c1d6780
Mel <jones.mel@gmail.com>
2023-03-14 17:34:01 +0100

    test: use toEqual matcher to test items * you can add multiple expectations to a single test

--- tdd-practice/test/stack.test.js
+++ tdd-practice/test/stack.test.js
@@ -12,8 +12,11 @@ describe('My Stack', () => {
 
     // make sure our code does whats expected
     // because top is empty when it's first created the value should be -1
-    // toBe is a jest matcher
+    // toBe is a jest matcher that checks for deep equality / object reference
     expect(stack.top).toBe(-1)
+
+    // toEqual is a jest matcher that checks for value equality
+    expect(stack.items).toEqual({})
   })
 
   it.todo('can push to the top')
